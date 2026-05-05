c941cc98dcfc500212f47245980a80c97d55979b
tsunamiqqq <128183552+tsunamiqqq@users.noreply.github.com>
2026-05-05 22:26:16 +0300

    Update pract3.12.cpp

--- pract3/pract3.12.cpp
+++ pract3/pract3.12.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+using namespace std;
 
+int main()
+{
+  int games = 110;
+  int players = 0;
+
+  for (int n = 1; n <= 100; n++)
+  {
+    if (n * (n - 1) == games)
+    {
+      players = n;
+      break;
+    }
+  }
+
+  cout << "Кількість шахістів: " << players << endl;
+
+  return 0;
+}
