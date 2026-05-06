bda9a15fead3301f4628e2e2f5621737377c5ff2
tsunamiqqq <128183552+tsunamiqqq@users.noreply.github.com>
2026-05-07 00:17:09 +0300

    Update pract4.21.cpp

--- pract4/pract4.21.cpp
+++ pract4/pract4.21.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    int total = 6 * 6 * 6;
+
+    int cubeA = 1;
+    int cubeB = 6;
+
+    double probabilityA = (double)cubeA / total;
+    double probabilityB = (double)cubeB / total;
+
+    cout << "Усього можливих результатів: " << total << endl;
+
+    cout << "1) Ймовірність того, що на кожному кубику випало 5 очок: "
+         << cubeA << "/" << total << " = " << probabilityA << endl;
+
+    cout << "2) Ймовірність того, що на всіх кубиках випала однакова кількість очок: "
+         << cubeB << "/" << total << " = " << probabilityB << endl;
+
+    return 0;
+}
