cc480a13649a08c7ba7dc0b2d2725407ef2d2747
tsunamiqqq <128183552+tsunamiqqq@users.noreply.github.com>
2026-05-07 00:09:57 +0300

    Add bus travel options calculation in pract3.11.cpp

--- pract3/pract3.11.cpp
+++ pract3/pract3.11.cpp
@@ -1,1 +1,13 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    int BorshchahivkaPodil = 4;
+    int PodilTeremky = 2;
+
+    int totalResult = BorshchahivkaPodil * PodilTeremky;
+
+    cout << "Кількість варіантів автобусної подорожі: " << totalResult << endl;
+
+    return 0;
+}
